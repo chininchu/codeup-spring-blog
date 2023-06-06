f92fc687db1ac92b23a11505134cb59be945c417
chininchu <120990876+chininchu@users.noreply.github.com>
2023-06-06 15:44:37 -0500

    Passing data to views

--- src/main/java/com/codeup/codeupspringblog/Controllers/RollDice.java
+++ src/main/java/com/codeup/codeupspringblog/Controllers/RollDice.java
@@ -2,10 +2,13 @@ package com.codeup.codeupspringblog.Controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
 
 @Controller
 public class RollDice {
@@ -13,24 +16,39 @@ public class RollDice {
 
     @GetMapping("/roll-dice")
 
-    public String showDiceForm(){
+    public String showDiceForm() {
 
         return "roll-dice";
 
 
+    }
+
+
+    @GetMapping("/roll-dice/{n}")
+
+    public String showResults(Model model, @PathVariable int n) {
+
+        int random = new Random().nextInt(7);
+
+        if (n == random) {
+
+            model.addAttribute("correct", true);
+
+
+        } else {
+
+            model.addAttribute("incorrect", true);
+
+
+        }
+
+        System.out.println(random);
+
+
+        return "show-results";
 
 
     }
-
-    @PostMapping(/roll)
-
-
-
-
-
-
-
-
 
 
 }

ed9601f8b5511e35aade969756c1ce0e59e3293e
chininchu <120990876+chininchu@users.noreply.github.com>
2023-06-12 12:19:34 -0500

    Created a Login and Logout form

--- src/main/java/com/codeup/codeupspringblog/Controllers/AuthenticationController.java
+++ src/main/java/com/codeup/codeupspringblog/Controllers/AuthenticationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class AuthenticationController {
 
-    @GetMapping
+    @GetMapping("/login")
 
 
     public String showLoginForm() {
